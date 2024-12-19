82cb48f51bb1f0a2f63f04a3efbb19b1205f0de2
kwasibaidoo <kwasi.baidoo@amalitech.com>
2024-12-19 16:48:05 +0000

    fix: Corrected App.java and module-info.java files

--- src/main/java/com/textprocessingscrum/App.java
+++ src/main/java/com/textprocessingscrum/App.java
@@ -17,7 +17,8 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("layout"), 800, 520);
+        stage.setTitle("Text Processing Tool");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,12 @@
 module com.textprocessingscrum {
     requires javafx.controls;
     requires javafx.fxml;
-    requires static lombok;
+    requires lombok;
 
     opens com.textprocessingscrum to javafx.fxml;
+    opens com.textprocessingscrum.controllers to javafx.fxml;
+    opens com.textprocessingscrum.models to javafx.base;
     exports com.textprocessingscrum;
+    exports com.textprocessingscrum.controllers;
+    exports com.textprocessingscrum.models;
 }
