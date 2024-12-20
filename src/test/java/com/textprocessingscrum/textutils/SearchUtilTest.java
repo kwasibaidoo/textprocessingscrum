301cd8797dcfffbeeda4b8efdd636d7452f21be0
nnyabe.chamamme <nnyabe.chamamme@amalitech.com>
2024-12-20 11:35:20 +0000

    test: Search functionality and controllers

--- src/test/java/com/textprocessingscrum/textutils/SearchUtilTest.java
+++ src/test/java/com/textprocessingscrum/textutils/SearchUtilTest.java
@@ -1,12 +1,44 @@
 package com.textprocessingscrum.textutils;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Test;
+import java.util.List;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+@ExtendWith(MockitoExtension.class)
 public class SearchUtilTest {
+
+    @InjectMocks
+    private SearchUtil searchUtil;
+
     @Test
-    void testSearch() {
-        assertTrue(true);
+    void testSearch_caseSensitive() {
+        List<List<Integer>> positions = searchUtil.search("regex", "regex", true);
+        assertFalse(positions.isEmpty(), "Positions list should not be empty");
+        assertEquals(1, positions.size(), "There should be one match");
+        assertEquals(0, positions.getFirst().get(0).intValue(), "Start index should be 0");
+        assertEquals(5, positions.getFirst().get(1).intValue(), "End index should be 4");
+    }
+
+    @Test
+    void testSearch_caseInsensitive() {
+        List<List<Integer>> positions = searchUtil.search("regex", "regex", false);
+        assertFalse(positions.isEmpty(), "Positions list should not be empty");
+        assertEquals(1, positions.size(), "There should be one match");
+        assertEquals(0, positions.getFirst().get(0).intValue(), "Start index should be 0");
+        assertEquals(5, positions.getFirst().get(1).intValue(), "End index should be 4");
+    }
+
+    @Test
+    void testSearch_noMatches() {
+        List<List<Integer>> positions = searchUtil.search("regex", "text", true);
+        assertTrue(positions.isEmpty(), "Positions list should be empty");
     }
 }
